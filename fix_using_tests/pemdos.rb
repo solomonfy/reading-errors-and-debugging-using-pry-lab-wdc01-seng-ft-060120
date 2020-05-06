d4c02e44744c94e170a371a0cb8aeb73d9055f1a
SOLOMON YISMAW <fantahunsolomon@gmail.com>
2020-05-06 18:49:08 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -4,7 +4,7 @@ require "pry"
 
 def snake_it_up(string)
   if string[0] == "s"
-    "s" * 10 + string
+    "s" * 10 + string # it was 10 * "s" + string ( the integer can't be multiplied s times)
   else
     string
   end
